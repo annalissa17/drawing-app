e4dc9325e9becff88d50c4f6732574224e41edff
annalissa17 <annalissa.laroche15@gmail.com>
2025-04-25 11:27:21 -0400

    selection d'un élément quand on est dessus

--- src/App.js
+++ src/App.js
@@ -10,20 +10,48 @@ function createElement(x1, y1, x2, y2, type) {
     type ==='line'
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2-x1, y2-y1)
-  return{ x1, y1, x2, y2, roughElement} 
+  return{ x1, y1, x2, y2, type, roughElement} 
+}
+
+const isWithinElement = (x, y, element) =>{
+  const {type, x1, x2, y1, y2} = element
+
+  if(type === 'rectangle'){
+    const minX = Math.min(x1, x2)
+    const maxX = Math.max(x1, x2)
+    const minY = Math.min(y1, y2)
+    const maxY = Math.max(y1, y2)
+
+    return x >= minX && x <= maxX && y >= minY && y <= maxY
+  }
+  else{
+    const a = {x1, y1}
+    const b = {x2, y2}
+    const c = {x, y}
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c))
+
+    return Math.abs(offset) < 1
+  }
+}
+const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+
+
+const getElementAtPosition = (x, y, elements) =>{
+  return elements.find(element => isWithinElement(x, y, element))
 }
 
 const App = () => {
 
   const [elements, setElements] = useState([])
-  const [drawing, setDrawing] = useState(false)
-  const [elementType, setElementType]= useState('line')
+  const [action, setAction] = useState('none')
+  const [tool, setTool]= useState('line')
 
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas')
     const context= canvas.getContext('2d')
 
     context.clearRect(0, 0, canvas.width, canvas.height)
+    
 
     const roughCanvas = rough.canvas(canvas)
 
@@ -31,46 +59,66 @@ const App = () => {
   }, [elements]);
 
   const handleMouseDown = (event) => {
-    setDrawing(true)
-
     const { clientX, clientY } = event
-    const element = createElement(clientX, clientY, clientX, clientY, elementType)
-    setElements(prevState => [...prevState, element])
+    if(tool === 'selection'){
+      const element = getElementAtPosition(clientX, clientY, elements)
+      if(element){
+        setAction('moving')
+      }
+    }
+
+    else{
+      const element = createElement(clientX, clientY, clientX, clientY, tool)
+      setElements(prevState => [...prevState, element])
+
+      setAction('drawing')
+    }
   }
 
   const handleMouseMove = (event) => {
-    if(!drawing) return
+    if(action === 'drawing'){
+      const {clientX, clientY} = event
 
-    const {clientX, clientY} = event
-
-    const index = elements.length -1
-    const {x1, y1} = elements[index]
-    const updatedElement = createElement(x1, y1, clientX, clientY, elementType)
-    
-    const elementsCopy = [...elements]
-    elementsCopy[index] = updatedElement
-    setElements(elementsCopy)
+      const index = elements.length -1
+      const {x1, y1} = elements[index]
+      const updatedElement = createElement(x1, y1, clientX, clientY, tool)
+      
+      const elementsCopy = [...elements]
+      elementsCopy[index] = updatedElement
+      setElements(elementsCopy)
+    }
   }
 
   const handleMouseUp = (event) => {
-    setDrawing(false)
+    setAction('none')
   }
 
   return (
-    <div>
+    <div style={{color: 'white'}}>
       <div style={{position:'fixed'}}>
         <input
          type='radio'
-         id='line'
-         checked={elementType === 'line'}
-         onChange={() => setElementType('line')}
+         id='selection'
+         checked={tool === 'selection'}
+         onChange={() => setTool('selection')}
          />
+         <label htmlFor='selection'>Selection</label>
+
+        <input
+         type='radio'
+         id='line'
+         checked={tool === 'line'}
+         onChange={() => setTool('line')}
+         />
+         <label htmlFor='line'>Line</label>
+
          <input
          type='radio'
          id='rectangle'
-         checked={elementType === 'rectangle'}
-         onChange={() => setElementType('rectangle')}
+         checked={tool === 'rectangle'}
+         onChange={() => setTool('rectangle')}
          />
+         <label htmlFor='rectangle'>Rectangle</label>
       </div>
       <canvas id='canvas' 
           style={{backgroundColor: '#3b3a39'}} 
